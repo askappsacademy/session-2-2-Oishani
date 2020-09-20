b6bb577bb9c8fc946ebcd4b1bd5942429c4889e3
Oishani Ganguly <oishani.ganguly@gmail.com>
2020-09-20 18:14:39 +0000

    Changed p tag text and background color

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const run = function () {
-    // Exercise 2
+  // Exercise 2
 
-    // Exercise 3 - Extra Credit
-    const fruits = ['Apple', 'Banana', 'Orange'];
-    
-    // hint 1: how do we loop through items in JavaScript
+  document.getElementById('ptag-id').innerText = 'Hello World!';
 
-    // hint 2: use how do we create an element and append an element?
-}
+  document.getElementById('ptag-id').style.backgroundColor = 'red';
 
-run()
+  // Exercise 3 - Extra Credit
+  const fruits = ['Apple', 'Banana', 'Orange'];
+
+  // hint 1: how do we loop through items in JavaScript
+
+  // hint 2: use how do we create an element and append an element?
+};
+
+run();
